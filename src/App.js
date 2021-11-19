65c86480bdd0b3dcc397d48888b0672903bfbf1b
Darius Loftis <dloft9@gmail.com>
2021-11-19 09:31:49 -0800

    set up of sass, installed packages, moved and created files and folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./styles/App.css";
+import "./App.scss";
 import RouterContainer from "./containers/RouterContainer";
 
 function App() {

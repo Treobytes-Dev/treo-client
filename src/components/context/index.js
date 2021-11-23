3f3045db66836a156ac5fc4de9e3bfb8005e2d01
Darius Loftis <dloft9@gmail.com>
2021-11-23 15:06:17 -0800

    removed old files to update to PWA bundle

--- src/components/context/index.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-export const AppContext = React.createContext();
-
-export const Provider = (props) => {
-  return <AppContext.Provider value={{}}>{props.children}</AppContext.Provider>;
-};

38b93c1010d7af22753da6cb2ff205e296407eb0
Darius Loftis <dloft9@gmail.com>
2024-03-01 18:49:21 -0800

    created copyBlock component, updated helpCenter page

--- src/pages/help-center.js
+++ src/pages/help-center.js
@@ -6,7 +6,7 @@ import Head from 'next/head';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 import Status from '../components/Status';
-import { TemplateIntro } from '../components/TemplateIntro';
+import { CopyBlock } from '../components/CopyBlock';
 
 const HelpCenter = ({ componentName }) => {
 	const head = () => (
@@ -26,6 +26,19 @@ const HelpCenter = ({ componentName }) => {
 			<meta property='og:image:secure_url' content='' />
 		</Head>
 	);
+
+	const listItems = [
+		{
+			id: 1,
+			text: '- learning theory and working on projects that apply this knowledge',
+		},
+		{ id: 2, text: '- classroom learning, breaks, and fun activities' },
+		{
+			id: 3,
+			text: '- a safe and healthy learning environment for all students',
+		},
+	];
+
 	return (
 		<>
 			{head()}
@@ -33,12 +46,42 @@ const HelpCenter = ({ componentName }) => {
 			<div className={`page ${componentName}`}>
 				{/* <TemplateIntro
 					heroImage={mockHeroImage}
-					heroHeader={mockHeroHeader2}
-					heroSubHeader={mockHeroHeader4}
-					introHeader={mockIntroHeader}
-					introBody={mockIntroParagraph}
-					scrollToTarget={scrollToTarget}
+					heroHeader='Help Center'
 				/> */}
+
+				<div className='container'>
+					<h2 className='header-two'>FAQs</h2>
+					<CopyBlock
+						header='Why should my student join a Treobytes program?'
+						copyPrimary='We take pride in always improving our programs and developing new offerings to students that keep up with industry standards. Technology is always getting better, and we are too! Our staff are STEM college students with a passion for preparing the next generation for the challenges they will likely face.'
+					/>
+					<CopyBlock
+						header='What can me and my student expect from a Treobytes experience?'
+						copyPrimary='Our programs provide an optimal balance of:'
+						listItem={listItems.map((item) => (
+							<li className='list-item' key={item.id}>
+								{item.text}
+							</li>
+						))}
+					/>
+					<CopyBlock
+						header='Who can speak with if I have a question about a Treobytes program?'
+						copyPrimary='Reach out to us at info@treobytes.com or 619.333.8589'
+					/>
+					<CopyBlock
+						header='What are the student to facilitator ratios?'
+						copyPrimary='The most dense ratio would be 12 students for each facilitator. Often our programs are close to 6 students per facilitator.'
+					/>
+					<CopyBlock
+						header='What is provided to the students?'
+						copyPrimary='Snacks and all materials for the course will be provided. For day camps, lunch will also be provided.'
+					/>
+					<CopyBlock
+						header='What is Treobytes looking to achieve?'
+						copyPrimary='We strive to ensure that as many students as possible are introduced to the world of STEM. An unfortunate reality is that students who would excel in the STEM workforce do not choose it because they were never introduced to the field or never considered it open to them. We want to provides students an informed choice when deciding what to purse after high school, which includes a possible STEM career.'
+					/>{' '}
+				</div>
+
 				<Footer />
 			</div>
 		</>

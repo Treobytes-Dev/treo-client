c5700e6d17338cbb0915f065de4f4f830712594a
Darius Loftis <dloft9@gmail.com>
2024-03-05 17:32:32 -0800

    styles and mark up for leadership page

--- src/pages/about/leadership.js
+++ src/pages/about/leadership.js
@@ -6,6 +6,26 @@ import Footer from '../../components/Footer';
 
 import { TemplateIntro } from '../../components/TemplateIntro';
 
+const Card = ({ title, name, contact, source }) => (
+	<div className='box'>
+		<div
+			className='avatar'
+			style={{
+				backgroundImage: 'url(' + source + ')',
+				backgroundRepeat: 'no-repeat',
+				backgroundPosition: 'center center',
+				backgroundSize: 'cover',
+				height: '50px',
+				width: '50px',
+				borderRadius: '50%',
+			}}
+		/>
+		<h5 className='header header-five title'>{title}</h5>
+		<h5 className='header header-five name'>{name}</h5>
+		<h6 className='header header-six contact'>{contact}</h6>
+	</div>
+);
+
 const Leadership = ({ componentName }) => {
 	const head = () => (
 		<Head>
@@ -26,14 +46,80 @@ const Leadership = ({ componentName }) => {
 	);
 
 	const hero =
-		'https://treo-assets.s3.us-west-2.amazonaws.com/about-us-hero.jpg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEIj%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMSJHMEUCIQCNSEvGdS3iuw2endcSCI8zPDX6Xkf%2BqZTs9Nuv4eyL2wIgXFC0IsGd52u%2FQeZw3xYZeW5JaGk1WCIf299PDMbJofgqhAMIgf%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARACGgw5NzgwMjc0NDI4NjgiDAK6VAfapWNdH9txwyrYAhYVDmVT1emvB9Q7SRJ4WDd6Zj9OW7w%2FvLQEuchtv0p%2BonUG9RGm5ITuxjwqlHaMRowjTFMm6pQ8dG%2FIuSGmuNL23qbky2UoYZNzJ8es0XIU86hxqWgxQba%2FK7GJUptDaA%2FQjZlbA7MoiO%2BG5l%2BaEs2lQkiBfLF5SwkHO5lpYmAlH%2F5D%2BXaO%2BI5fz9QSPUKGEAUGiiiMHr4Li9xQkpq8BLusgxmvi4YtJl8V%2BXbJVQEwKjZRVu96GSsk4ZSXUDxBEEIa8Gmd9MKxYQo78R2XIUq0DzdiZKtJF8oflCZRF8%2B088bHSikYzDNYxeL34RtQCiMbzZdAYMNYyg5iV61M8mu6wI9tv1fhYxiW1D%2BDtTkQ%2BsMd0brWrle%2B4B0t%2F62bjsD%2FkU2g8K318O4zghue6QVgkNT1PP9NdDZSI2eyQqFe1JVDEVhXzBhzuG4BmcfRJltaA0s8WklhMO%2B3ma8GOrMCMqSEr8mbOCqI0ZO76gbJqwq71KGZq5W16fkuJlgq5SrPvCaeUmlGf4goG%2F2PsnIU7WoLDkPiLkaSFVX5EcVbJEZLjsn%2BqH8JMewpf6cAzpSdOLE1JvbzhVk97zXrp1FPqseImb5RL24AsEFlwmHNAQ5Uvt%2BBsXn8nTwS7Q%2Bdwo6gXEBBfZ4Lqr%2FN9Dm982X%2BuKYi%2BPWXNt7Tp7Mxle8LcvppYID9MGDcVAhqaoPuGMiehAqkpBA%2FTK9Ur0FTAZlvgfw%2FijUE0tcnoUq49ftP3qWCi4%2Fgkt%2FBc8onjCJRpn1Crl2F2shB93zZutCrDDOPKSf%2FgZ2jiI7I4MM2mLjtNEA4qIeMUfoAcSHfOg0GFcLxFpiJXH1mscr%2Fpy6bqBvALm6ItCpcq%2FqCi%2BvrtGiKkcf2Yg%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240304T234109Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA6HNX2OK2MPCXVM7P%2F20240304%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Signature=c68d6fe8dc721380d2030169e0ce4312b40aa0928f868f83ac3cb24dd45a0cd8';
+		'https://treo-assets.s3.us-west-2.amazonaws.com/leadership-hero.jpeg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEKH%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMSJHMEUCIQC8yroY9bhOA1h7r5sd7yFHwW1lfK23ExrLZwoN3pA2lQIgBzFoVfmuvwUKNpOXn2Q2rrySsxfs4KH%2F7MCcjimstn4qhAMImv%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARACGgw5NzgwMjc0NDI4NjgiDDO2AFUe1wceOcJLWirYAgrDq8TJGwJchHSt54%2BE5dBp5Dr%2F7Szm%2BTS4CjRUo8SB0UKZmEibDIL53UAMLD4HPPuE2dQ4xzA4a41Do6gjnQS9CD3nMJq1PGziT3gppqG6a7kqGUqD1lGEjZAxL%2FoTRMhh3AVN2GeUctVkZCDOODX8xhdc%2FZJi0X3GuE4Wxxxf7zDUX2%2FjQHItIMMyJceD7AiBrNCPOOs4W%2FuuhvYU0r6APylnOokq8dOt83UFe3%2F6DrHbIZVhoA8i4kJJ8wL17Xx8nmBX4iBq6FCrUXq7zndus9TgQ%2FqJXrqsA9HwfTY%2BltmkiYkW3oN8mIZcIWu8dJfAF%2BMbBUEmEfFBjfvVIO5%2B8f5WWKxk25zQF43gsMCADiUe%2BH9J9iOpPje5Q4cOjs0ohcGC%2FK8Ozwoz56tNbCJ7V7DIFh8%2Frv0OZH6VGmVoJbz46pLsJ8Si6KWzqcOA1wxZCowrebwHMJ%2F3nq8GOrMCoh70G2nLOjMfYlvnXfjSh%2FQs%2FI%2Fht4SJ2ka5p3F2R7DdqJ85%2FZKDppMfm4t52DRuo68cwhzSLL0Zjb3oMjoGRvt3zIW8RAthzUjnK8I5nFCUP3hE5eFF1bjAq38kVVxuszg1FfZyYG9uZA%2FhxwfJoYac4lfx%2FPW%2BsE4wOx0UDOfQR2Ru6X%2FRXxe3ONOUD%2BoMPR0%2FMNK7J71jOWKkMHjSC6TeJMpnCRhnnZcWLVK1G3pBXKqD4iFBLerNrnLKNG9zfPW%2FwiO3M85eB6oKT%2B9hpKfEqOrL3w%2FNVLeOD%2BU2AhDY%2BxucwMTvsGm7O1q%2FDy57gasXylI6myZcx7Z%2FH1FFcxauRpl%2B5dGVJdHzFsy3C6Uw3IVYYnva9U1bCoKE0kz9OPDod4qcsYMLyNaAETr3P6eqUg%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240306T004307Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA6HNX2OK2FUCSS5EC%2F20240306%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Signature=e559101f97b6dbfeeb347decf130eadbe301160a0e8252727c67d1fb590d9458';
 
 	return (
 		<>
 			{head()}
 			<Navigation />
 			<div className={`page ${componentName}`}>
-				<TemplateIntro heroImage={hero} heroHeader='Leadership' />
+				<TemplateIntro
+					heroImage={hero}
+					heroHeader='Leadership'
+					introHeader='Management Team'
+					introBody='Meet our team working to achieve equity in the STEM workforce!'
+				/>
+
+				<div className='container'>
+					<div className='tier tier-1'>
+						<Card
+							title='Founder & CEO'
+							name='Ava Mason'
+							contact='ava@treobytes.com'
+							source='https://treo-assets.s3.us-west-2.amazonaws.com/avatar-ava.jpg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEKH%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMSJHMEUCIQC8yroY9bhOA1h7r5sd7yFHwW1lfK23ExrLZwoN3pA2lQIgBzFoVfmuvwUKNpOXn2Q2rrySsxfs4KH%2F7MCcjimstn4qhAMImv%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARACGgw5NzgwMjc0NDI4NjgiDDO2AFUe1wceOcJLWirYAgrDq8TJGwJchHSt54%2BE5dBp5Dr%2F7Szm%2BTS4CjRUo8SB0UKZmEibDIL53UAMLD4HPPuE2dQ4xzA4a41Do6gjnQS9CD3nMJq1PGziT3gppqG6a7kqGUqD1lGEjZAxL%2FoTRMhh3AVN2GeUctVkZCDOODX8xhdc%2FZJi0X3GuE4Wxxxf7zDUX2%2FjQHItIMMyJceD7AiBrNCPOOs4W%2FuuhvYU0r6APylnOokq8dOt83UFe3%2F6DrHbIZVhoA8i4kJJ8wL17Xx8nmBX4iBq6FCrUXq7zndus9TgQ%2FqJXrqsA9HwfTY%2BltmkiYkW3oN8mIZcIWu8dJfAF%2BMbBUEmEfFBjfvVIO5%2B8f5WWKxk25zQF43gsMCADiUe%2BH9J9iOpPje5Q4cOjs0ohcGC%2FK8Ozwoz56tNbCJ7V7DIFh8%2Frv0OZH6VGmVoJbz46pLsJ8Si6KWzqcOA1wxZCowrebwHMJ%2F3nq8GOrMCoh70G2nLOjMfYlvnXfjSh%2FQs%2FI%2Fht4SJ2ka5p3F2R7DdqJ85%2FZKDppMfm4t52DRuo68cwhzSLL0Zjb3oMjoGRvt3zIW8RAthzUjnK8I5nFCUP3hE5eFF1bjAq38kVVxuszg1FfZyYG9uZA%2FhxwfJoYac4lfx%2FPW%2BsE4wOx0UDOfQR2Ru6X%2FRXxe3ONOUD%2BoMPR0%2FMNK7J71jOWKkMHjSC6TeJMpnCRhnnZcWLVK1G3pBXKqD4iFBLerNrnLKNG9zfPW%2FwiO3M85eB6oKT%2B9hpKfEqOrL3w%2FNVLeOD%2BU2AhDY%2BxucwMTvsGm7O1q%2FDy57gasXylI6myZcx7Z%2FH1FFcxauRpl%2B5dGVJdHzFsy3C6Uw3IVYYnva9U1bCoKE0kz9OPDod4qcsYMLyNaAETr3P6eqUg%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240306T012342Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA6HNX2OK2FUCSS5EC%2F20240306%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Signature=a87d221bcf938dfd524a1ad7be767b848952bd31e8b1579e495f4df42e5dc6d3'
+							altTag='Ava Mason'
+						/>
+					</div>
+					<div className='tier tier-2'>
+						<Card
+							title='Director'
+							name='Antoine Serrano'
+							contact='antoines@treobytes.com'
+						/>
+					</div>
+					<div className='tier tier-3'>
+						<Card
+							title='Assistant Director'
+							name='Lauren Wisniewski'
+							contact='lauren@treobytes.com'
+						/>
+						<Card title='Operations Administrator' name='TBA' />
+					</div>
+					<div className='tier tier-4'>
+						<div className='column column-1'>
+							<h4 className='header header-four'>Marketing Team</h4>
+							<div className='cards'>
+								<Card
+									title='Marketing Director'
+									name='Jeremy Joseph'
+									contact='jjoseph@treobytes.com'
+								/>
+								<Card
+									title='Digital Marketing Specialist'
+									name='Sarai Gomez'
+									contact='sarai@treobytes.com'
+								/>
+							</div>
+						</div>
+
+						<div className='column column-2'>
+							<h4 className='header header-four'>Product Development</h4>
+							<div className='cards'>
+								<Card
+									title='Product Development'
+									name='Khalil Jackson'
+									contact='khalil@treobytes.com'
+								/>
+								<Card
+									title='Product Development'
+									name='Ryan Palmares'
+									contact='ryan@treobytes.com'
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<Footer />
 			</div>
 		</>
 	);

4c580823114198345ea8808c5b4718a3d51a66c5
Darius Loftis <dloft9@gmail.com>
2024-03-10 10:22:50 -0700

    added sections for donate

--- src/pages/partner-with-us/donate.js
+++ src/pages/partner-with-us/donate.js
@@ -78,9 +78,139 @@ const Donate = ({ componentName }) => {
 				</div>
 
 				<div className='container'>
-					<h2 className='header header-two'>
+					<h2 className='header header-two red'>
 						LET’S BUILD THE NEXT GENERATION OF STEM LEADERS TOGETHER!
 					</h2>
+				</div>
+
+				<div className='container'>
+					<div className='columns'>
+						{' '}
+						<div className='column col-1'>
+							<h3 className='header header-three red'>
+								EMPOWERING 25,000 STUDENTS IN STEM
+							</h3>
+							<p className='paragraph'>
+								We are so proud that we've touched the lives of over 25,000
+								students, guiding them towards the vast horizon of the STEM
+								field. We focused on preparing students to meet college entrance
+								requirements gaps, to thrive in the college environment and the
+								STEM workforce.
+							</p>
+							<p className='paragraph'>
+								With your help, future young minds, brimming with potential,
+								will have the opportunity to chase their dreams.
+							</p>
+							<p className='paragraph red'>
+								Your donation will help us offer our programs to students at no
+								cost to them!
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div className='container'>
+					<div className='columns'>
+						<div className='column col-1'>
+							<h3 className='header header-three red'>
+								CREATING FUTURE LEADERS
+							</h3>
+							<p className='paragraph'>
+								In the eyes of every student, we see the spark of leadership and
+								progress waiting to ignite. With your support, we can nurture
+								this flame, guiding them toward a future where they can lead
+								with compassion, innovation, and integrity.
+							</p>
+
+							<p className='paragraph red'>
+								Each donation is a seed planted, promising a harvest of leaders
+								who will shape the world for the better.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div className='container'>
+					<div className='columns'>
+						{' '}
+						<div className='column col-1'>
+							<h3 className='header header-three red'>
+								PUTTING COMMUNITY FIRST
+							</h3>
+							<p className='paragraph'>
+								Our journey is intertwined with the communities we live in and
+								serve: young students, college students, parents, schools, and
+								industry.
+							</p>
+
+							<p className='paragraph'>
+								Every smile we see, and every success story we witness, is a
+								testament to the unwavering commitment of our facilitators, who
+								share in the hopes and aspirations of those they mentor.
+							</p>
+
+							<p className='paragraph red'>
+								Together, we work to build a stronger, more resilient community.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div className='container'>
+					<div className='columns'>
+						<div className='column col-1'>
+							<h3 className='header header-three red'>
+								WALKING THE SAME STREETS
+							</h3>
+							<p className='paragraph'>
+								Our journey begins where the pavement meets the heartache of
+								underprivileged neighborhoods. Here, we find our purpose,
+								walking hand in hand with those who deserve every opportunity to
+								thrive.
+							</p>
+
+							<p className='paragraph red'>
+								Your support helps us pave the way for a brighter future where
+								every child can walk the path of success.
+							</p>
+						</div>
+
+						<div className='column col-2'>
+							<img
+								className='img'
+								src='https://res.cloudinary.com/dtjvswb7v/image/upload/v1710010479/donate-image-1_kob3iq.jpg'
+								alt='walking the streets image'
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div className='container'>
+					<div className='columns'>
+						<div className='column col-1'>
+							<img
+								className='img'
+								src='https://res.cloudinary.com/dtjvswb7v/image/upload/v1710010479/donate-image-1_kob3iq.jpg'
+								alt='walking the streets image'
+							/>
+							<h3 className='header header-three red'>
+								FOR THE PRIcCE OF A CUP OF COFFEE
+							</h3>
+							<p className='paragraph'>
+								We love our lattes. We also love knowing that our youth are
+								prepared for the future. $5 matters. You matter. Let’s do this.
+							</p>
+						</div>
+
+						<div className='column col-2'>
+							<img
+								className='img'
+								src='https://res.cloudinary.com/dtjvswb7v/image/upload/v1710010479/donate-image-1_kob3iq.jpg'
+								alt='walking the streets image'
+							/>
+							<button className='submit'>Donate</button>
+						</div>
+					</div>
 				</div>
 				<Footer />
 			</div>

23abd45248f70e97c1fb73b61d38bdd167fdde21
Darius Loftis <dloft9@gmail.com>
2024-03-15 11:12:33 -0700

    markup and styles for introToCamp componenet

--- src/pages/programs/summer-of-stem.js
+++ src/pages/programs/summer-of-stem.js
@@ -5,9 +5,9 @@ import Navigation from '../../components/Navigation';
 import Footer from '../../components/Footer';
 import Button from '../../components/Button';
 import ProgramDates from '../../components/ProgramDates';
+import IntroToCamp from '../../components/IntroToCamp';
 
 import { TemplateIntro } from '../../components/TemplateIntro';
-import Link from 'next/link';
 
 const SummerOfSTEM = ({ componentName }) => {
 	const head = () => (
@@ -51,6 +51,7 @@ const SummerOfSTEM = ({ componentName }) => {
 						<Button children='JOIN TODAY!' additionalClass='red' />
 					</a>
 				</div>
+
 				<ProgramDates
 					image1='https://res.cloudinary.com/dtjvswb7v/image/upload/v1710429934/summer-of-stem-1_n2voyq.jpg'
 					image2='https://res.cloudinary.com/dtjvswb7v/image/upload/v1710429934/summer-of-stem-2_zqfgjx.jpg'
@@ -81,11 +82,17 @@ const SummerOfSTEM = ({ componentName }) => {
 							<p className='paragraph'>June 17th - June 21st</p>
 							<p className='paragraph'>June 24th - June 28th</p>
 							<p className='paragraph'>July 1st - July 5th **</p>
-							<p className='paragraph'>** No Camp on July 4th</p>
+							<p className='paragraph'>
+								<strong style={{ fontWeight: '700' }}>
+									** No Camp on July 4th
+								</strong>
+							</p>
 						</>
 					}
 					contentTime={<p className='paragraph'>9:00am -4:00pm</p>}
 				/>
+
+				<IntroToCamp />
 				<Footer />
 			</div>
 		</>

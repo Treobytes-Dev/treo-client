c761988911680e4e5dc0f090e416bef6d879d3b7
Darius Loftis <dloft9@gmail.com>
2024-03-21 09:38:26 -0700

    updated compeonts styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,89 +8,11 @@ import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 import Status from '../components/Status';
 
-import DiagramLevels from '../components/DiagramLevels';
-
 import { TemplateIntro } from '../components/TemplateIntro';
-import { TemplateSecondary } from '../components/TemplateSecondary';
-import { TemplateThird } from '../components/TemplateThird';
-import { TemplateFourth } from '../components/TemplateFourth';
-
-// icons
-import { Loader } from '../assets/icons/Loader';
-import Robot from '../assets/icons/Robot';
-import PartnerCollab from '../assets/icons/PartnerCollab';
 
 // mocking hero
 export const mockHeroImage =
 	'http://treobytes.org/wp-content/uploads/2019/09/Girl-STEAM.jpg';
-
-export const mockHeroHeader2 = '6th - 8th Grade';
-export const mockHeroHeader4 = 'Vocational STEM Education';
-
-export const mockIntroHeader = 'Career Aspirations Being in Middle School';
-export const mockIntroParagraph = '';
-
-// mocking images
-export const mockSingleImage =
-	'https://i0.wp.com/treobytes.org/wp-content/uploads/2020/03/AdobeStock_267992921-scaled.jpeg?resize=360%2C240&ssl=1';
-
-export const mockEngineeringKitPhoto =
-	'https://i0.wp.com/treobytes.org/wp-content/uploads/2021/07/AdobeStock_236050835-scaled.jpeg?fit=750%2C500&ssl=1';
-
-export const mockThumbnails = [
-	'https://i0.wp.com/treobytes.org/wp-content/uploads/2019/10/AdobeStock_269246946.jpeg?resize=360%2C240&ssl=1',
-
-	'https://i0.wp.com/treobytes.org/wp-content/uploads/2019/09/shutterstock_391124713.jpg?resize=360%2C240&ssl=1',
-
-	'https://i0.wp.com/treobytes.org/wp-content/uploads/2019/09/shutterstock_660256189.jpg?resize=360%2C240&ssl=1',
-];
-
-// mocking module
-export const mockRowOne = [
-	{
-		number: '01',
-		text: '	Proactively engineer real-time bandwidth for bricks-and-clicks quality vector Assertively evisculate interdependent leadership skills through clicks-and-mortar.',
-		classModifier: 'one',
-	},
-	{
-		number: '02',
-		text: 'Monotonectally synergize professional value before equity invested						e-tailers. Seamlessly supply performance based portals with quality.',
-		classModifier: 'two',
-	},
-	{
-		number: '03',
-		text: 'Dynamically brand synergistic total linkage and tactical sources.      Synergistically negotiate value-added mindshare without fully tested.',
-		classModifier: 'three',
-	},
-	{
-		number: '04',
-		text: 'Synergistically reconceptualize diverse sources rather than open-source quality vectors. Progressively brand parallel customer service without.',
-		classModifier: 'four',
-	},
-];
-
-export const mockRowTwo = [
-	{
-		number: '01',
-		text: 'Basic "RoBo" Motion',
-		classModifier: 'one',
-	},
-	{
-		number: '02',
-		text: 'Technical Programming',
-		classModifier: 'two',
-	},
-	{
-		number: '03',
-		text: 'Prototyping',
-		classModifier: 'three',
-	},
-	{
-		number: '04',
-		text: 'Project Completion Knowledge Assessment',
-		classModifier: 'four',
-	},
-];
 
 const Home = ({ componentName }) => {
 	const [state] = useContext(AppContext);
@@ -139,7 +61,9 @@ const Home = ({ componentName }) => {
 					))}
 
 				<TemplateIntro
-					heroImage={mockHeroImage}
+					heroImage={
+						'https://res.cloudinary.com/dtjvswb7v/image/upload/v1711038021/home-hero_vwqlwz.webp'
+					}
 					scrollToTarget={scrollToTarget}
 				/>
 
